bc79b5c8e5dd41921bcb2a5f0781db00c79aadbb
Jason Lee <113562298+Vaten0x@users.noreply.github.com>
2023-06-17 15:24:47 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,29 @@
-<html>
-<head>
-<title>app.js</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<style type="text/css">
-.s0 { color: #cf8e6d;}
-.s1 { color: #bcbec4;}
-.s2 { color: #bcbec4;}
-.s3 { color: #6aab73;}
-</style>
-</head>
-<body bgcolor="#1e1f22">
-<table CELLSPACING=0 CELLPADDING=5 COLS=1 WIDTH="100%" BGCOLOR="#606060" >
-<tr><td><center>
-<font face="Arial, Helvetica" color="#000000">
-app.js</font>
-</center></td></tr></table>
-<pre><span class="s0">function </span><span class="s1">Function_Plank</span><span class="s2">() {</span>
-  <span class="s1">document</span><span class="s2">.</span><span class="s1">getElementById</span><span class="s2">(</span><span class="s3">'message'</span><span class="s2">)</span>
-      <span class="s2">.</span><span class="s1">innerHTML</span><span class="s2">=</span><span class="s3">&quot;You want improvements doing: Plank&quot;</span><span class="s2">;</span>
-<span class="s2">}</span>
+function Function_Plank() {
+  document.getElementById('message')
+      .innerHTML="You want improvements doing: Plank";
+}
 
-<span class="s0">function </span><span class="s1">Function_Pushup</span><span class="s2">() {</span>
-  <span class="s1">document</span><span class="s2">.</span><span class="s1">getElementById</span><span class="s2">(</span><span class="s3">'message'</span><span class="s2">)</span>
-      <span class="s2">.</span><span class="s1">innerHTML</span><span class="s2">=</span><span class="s3">&quot;You want improvements doing: Push up&quot;</span><span class="s2">;</span>
-<span class="s2">}</span>
+function Function_Pushup() {
+  document.getElementById('message')
+      .innerHTML="You want improvements doing: Push up";
+}
 
-<span class="s0">function </span><span class="s1">Function_Squats</span><span class="s2">() {</span>
-  <span class="s1">document</span><span class="s2">.</span><span class="s1">getElementById</span><span class="s2">(</span><span class="s3">'message'</span><span class="s2">)</span>
-      <span class="s2">.</span><span class="s1">innerHTML</span><span class="s2">=</span><span class="s3">&quot;You want improvements doing: Squats&quot;</span><span class="s2">;</span>
-<span class="s2">}</span>
+function Function_Squats() {
+  document.getElementById('message')
+      .innerHTML="You want improvements doing: Squats";
+}
 
-<span class="s0">function </span><span class="s1">Function_JumpingJacks</span><span class="s2">() {</span>
-  <span class="s1">document</span><span class="s2">.</span><span class="s1">getElementById</span><span class="s2">(</span><span class="s3">'message'</span><span class="s2">)</span>
-      <span class="s2">.</span><span class="s1">innerHTML</span><span class="s2">=</span><span class="s3">&quot;You want improvements doing: Jumping Jacks&quot;</span><span class="s2">;</span>
-<span class="s2">}</span>
+function Function_JumpingJacks() {
+  document.getElementById('message')
+      .innerHTML="You want improvements doing: Jumping Jacks";
+}
 
-<span class="s0">function </span><span class="s1">Function_Situp</span><span class="s2">() {</span>
-  <span class="s1">document</span><span class="s2">.</span><span class="s1">getElementById</span><span class="s2">(</span><span class="s3">'message'</span><span class="s2">)</span>
-      <span class="s2">.</span><span class="s1">innerHTML</span><span class="s2">=</span><span class="s3">&quot;You want improvements doing: Sit up&quot;</span><span class="s2">;</span>
-<span class="s2">}</span>
+function Function_Situp() {
+  document.getElementById('message')
+      .innerHTML="You want improvements doing: Sit up";
+}
 
-<span class="s0">function </span><span class="s1">Function_BenchPress</span><span class="s2">() {</span>
-  <span class="s1">document</span><span class="s2">.</span><span class="s1">getElementById</span><span class="s2">(</span><span class="s3">'message'</span><span class="s2">)</span>
-      <span class="s2">.</span><span class="s1">innerHTML</span><span class="s2">=</span><span class="s3">&quot;You want improvements doing: Bench Press&quot;</span><span class="s2">;</span>
-<span class="s2">}</span>
-</pre>
-</body>
-</html>
+function Function_BenchPress() {
+  document.getElementById('message')
+      .innerHTML="You want improvements doing: Bench Press";
+}
